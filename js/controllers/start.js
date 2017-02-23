0629679f69d3c3eb48520474f11caef263dc648e
Albina-art <albi.valiullina@yandex.ru>
2017-02-23 18:01:30 +0300

    click metronome modif

--- js/controllers/start.js
+++ js/controllers/start.js
@@ -1,12 +1,11 @@
 window.controller = window.controller || {}
 controller.start = () => {
   show(HAML.start(), 'app')
-  document.getElementById('metronome').addEventListener('click', () => {
+  document.getElementById('metronome').addEventListener('click', (event) => {
+    event.target.setAttribute('disabled', true)
+    debugger
+    document.getElementById('metronome').setAttribute('disabled', true)
+    console.log("fsf")
     controller.metronome()
   })
-  // help.setHandler('start', 'click', (event) => {
-  //   event.target.setAttribute('disabled', true)
-  //   select = document.getElementById('start')
-  //   select.setAttribute('disabled', true)
-  // })
 }
