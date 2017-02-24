b4c27e4d506ce1dbe2e3ed33cd4d421f63199961
Albina-art <albi.valiullina@yandex.ru>
2017-02-24 23:06:36 +0300

    test + giihub

--- app/main.js
+++ app/main.js
@@ -18,8 +18,8 @@ function createWindow () {
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
-   mainWindow.setMenu(null)
-   mainWindow.setResizable(false)
+  mainWindow.setMenu(null)
+  mainWindow.setResizable(false)
   
 
 
