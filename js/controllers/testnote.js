80eecb80edcc41e28907871e433055c1a73fa2bb
AnatolyShirykalov <pipocavsobake@gmail.com>
2017-03-02 15:50:19 +0300

    test schema

--- js/controllers/testnote.js
+++ js/controllers/testnote.js
@@ -1,4 +1,21 @@
 window.controller = window.controller || {}
 controller.testnote = () => {
-	show(HAML.testnote())
+  let notes = ['c', 'cis', 'd', 'es', 'e', 'f', 'fis', 'g', 'as', 'a', 'b', 'h']
+  show(    HAML.testnote({notes} )  )
+  let sh_notes = _.shuffle(notes)
+  let i = 0
+  let play = (note)=>{console.log(note)}
+  $('.note').click((e) => {
+    if (e.target.id == `but_${sh_notes[i]}`) {
+      console.log("правильный ответ")
+      if (i + 1 ==sh_notes.length) {
+        console.log("игра окончена")
+      } else {
+        play(sh_notes[++i])
+      }
+    } else {
+      console.log("неправильный ответ")
+    }
+  })
+  play(sh_notes[i])
 }
